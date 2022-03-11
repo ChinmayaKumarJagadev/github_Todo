63a5729e6ed6e9eaff23bde733761caa44a0bdbd
ChinmayaJagadev <chinmaya.jagadev@bluelabellabs.com>
2022-03-11 12:39:16 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,82 @@
-import React,{useState,useEffect} from 'react'
-import TodoList from './component/TodoList/TodoList';
-import './App.css';
-import {InputGroup,Button,FormControl} from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import TodoList from "./component/TodoList/TodoList";
+import "./App.css";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
 
-const App = () =>{
+const App = () => {
+  const [input, setInput] = useState();
+  const [todos, setTodos] = useState([]);
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
 
-  const[input, setInput] = useState();
-  const [todos,setTodos] = useState([])
-  const handleChange = (event) =>{
-    setInput(event.target.value)
-  }
+  const handleClick = () => {
+    console.log(input);
+    setTodos((previousState) => {
+      return [...previousState, input];
+    });
+    setInput("");
+  };
 
-  const handleClick = () =>{
-    console.log(input)
-    setTodos((previousState) =>{
-      return([...previousState, input])
-    })
-    setInput("")
-  }
+  const handleDeleteTodo = (id) => {
+    console.log(id);
+    if (window.confirm("Are You sure to delete selected Todo")) {
+      setTodos((prevState) => {
+        // console.log(prevState)
+        return prevState.filter((todoList, index) => {
+          console.log(index);
+          console.log(todoList);
+          return index !== id;
+        });
+      });
+    }
+  };
 
- const handleDeleteTodo  = (id) =>{
-  console.log(id)
-  if(window.confirm('Are You sure to delete selected Todo')){
-    setTodos((prevState) =>{
-      // console.log(prevState)
-      return prevState.filter((todoList, index) =>{
-        console.log(index)
-        console.log(todoList)
-        return index !== id
-      })
-  })
-  } 
-}
+  useEffect(() => {
+    const data = localStorage.getItem("todoList");
+    if (data) {
+      setTodos(JSON.parse(data));
+    }
+  }, []);
 
-useEffect (() =>{
-  const data = localStorage.getItem("todoList") 
-  if(data){
-    setTodos(JSON.parse(data))
-  }
-},[])
-
-useEffect(() => {
-  localStorage.setItem( 'todoList' ,JSON.stringify(todos))
-}, [todos])
-
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div>
-    
-          <InputGroup className="mb-2" >
-                 <FormControl
-                  name="todo" 
-                  value={input || ""} 
-                  onChange={handleChange} placeholder= "Add Tasks" className="inputBox"
-                  placeholder="Add Task"
-                  aria-label="Add Task" />
-      
-           <Button disabled={!input} variant="outline-secondary" onClick={handleClick} className= "buttonDesign">ADD</Button>
-        </InputGroup>
-  
-     
+      <InputGroup className="mb-2">
+        <FormControl
+          name="todo"
+          value={input || ""}
+          onChange={handleChange}
+          placeholder="Add Tasks"
+          className="inputBox"
+          aria-label="Add Task"
+        />
 
-      {todos.map((todo,index)=>{
-         return(
-           <TodoList
-               key={index}
-               id={index}
-               allTodos={todo}
-               onClick={handleDeleteTodo}
-           />
-         )
+        <Button
+          disabled={!input}
+          variant="outline-secondary"
+          onClick={handleClick}
+          className="buttonDesign"
+        >
+          ADD
+        </Button>
+      </InputGroup>
+
+      {todos.map((todo, index) => {
+        return (
+          <TodoList
+            key={index}
+            id={index}
+            allTodos={todo}
+            onClick={handleDeleteTodo}
+          />
+        );
       })}
-  
     </div>
   );
-}
+};
 
 export default App;
